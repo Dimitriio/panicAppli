e7267f7abace73168dffa186792fd3ec20d6fef9
Daft_Spirit <joris.rat@gmail.com>
2015-12-04 05:11:33 +0100

    Math things !

--- src/kMeans/Run.java
+++ src/kMeans/Run.java
@@ -6,35 +6,50 @@ import java.util.Random;
 import model.User;
 
 public class Run {
-	private Random rand = new Random();
-	private int nbClusters = 2;
-	
-	public User generateUser() {
-		double x = new Double(rand.nextDouble());
-		double y = new Double(rand.nextDouble());
-		User res = new User(x, y);
-		return res;
-	}
-	
+
+	private final static double EARTH_RADIUS = 6378140.0;
+	private final static double DISTANCE_ALERT = 500.0; // distance between two
+														// people to be consider
+														// in the same group
+	private final static int NUMBER_TO_WARN = 20; // number of persons in the
+													// group for warning
+													// authorities
+
 	public double distance(User u1, User u2) {
+		//Spheric coordinates converted in cartesian coordinates
 		double dist = 0.0;
-		dist += Math.pow(u1.getLatitude() - u2.getLatitude(), 2);
-		dist += Math.pow(u1.getLongitude() - u2.getLongitude(), 2);
+		double x1 = (-this.EARTH_RADIUS)*Math.cos(u1.getLongitude())*Math.cos(u1.getLatitude());
+		double y1 = (-this.EARTH_RADIUS)*Math.cos(u1.getLatitude())*Math.sin(u1.getLongitude());
+		double z1 = this.EARTH_RADIUS*Math.sin(u1.getLatitude());
+		double x2 = (-this.EARTH_RADIUS)*Math.cos(u2.getLongitude())*Math.cos(u2.getLatitude());
+		double y2 = (-this.EARTH_RADIUS)*Math.cos(u2.getLatitude())*Math.sin(u2.getLongitude());
+		double z2 = this.EARTH_RADIUS*Math.sin(u2.getLatitude());
+		dist += Math.pow(x1 - x2, 2);
+		dist += Math.pow(y1 - y2, 2);
+		dist += Math.pow(z1 - z2, 2);
 		return Math.sqrt(dist);
 	}
-	
-	public User[] generateClusters() {
-		User[] res = new User[this.nbClusters];
-		for(int i = 0; i < res.length; ++i){
-			res[i] = this.generateUser();
+
+	public ArrayList<User> problemDetected(ArrayList<User> calls) {
+		ArrayList<User> copy = new ArrayList<User>(calls);
+		int[] proximity = new int[calls.size()];
+		for (int i = 0; i < calls.size(); ++i) {
+			copy.remove(i);
+			for (User user : copy) {
+				if (distance(calls.get(i), user) < this.DISTANCE_ALERT) {
+					proximity[i]++;
+				}
+			}
+			copy.add(i, calls.get(i));
 		}
-		
+
+		ArrayList<User> res = new ArrayList<User>();
+		for (int j = 0; j < proximity.length; ++j) {
+			if (proximity[j] >= this.NUMBER_TO_WARN) {
+				res.add(calls.get(j));
+			}
+		}
 		return res;
 	}
-	
-	public int[] clusterMap(ArrayList<User> calls) {
-		return null;
-	}
-	
-	
+
 }
